5c576f5ea1735ca41d8a32f2106c9cecfe2e3c73
Semyon Merkulov <semen.merkulov@gmail.com>
2019-03-23 17:45:31 +0700

    finished

--- web/script.js
+++ web/script.js
@@ -1,23 +1,68 @@
 var message = Vue.component('message', {
 	props: {
-		client: ,
-		text: ''
+		client: Boolean,
+		text: String
 	},
-	template: '<li :class="{ chat__msg-item: true, chat__msg-item_client: true }">{{ this.text }}</li>'
+	computed: {
+		classObject: function() {
+			return {
+				'chat__msg-item': true,
+				'chat__msg-item_client': this.client
+			}
+		} 
+	},
+	template: '<li :class="classObject">{{ this.text }}</li>'
 });
 
 new Vue({
 	el: "#app",
+	data: {
+		field: ''
+	},
 	components: { message },
 	methods: {
 		sendMsg: function() {
-			var ComponentClass = Vue.extend(message);
-      var instance = new ComponentClass({
-        propsData: { client: true, text: 'Test text' }
-      });
-      instance.$mount();
-      // console.log(this.message);
-			this.$refs.list.appendChild(instance.$el);
+			const msgComponent = Vue.extend(message);
+			const params = new URLSearchParams();
+
+
+			if (!this.isEmpty) {
+			// rendering user's message
+	      let userMsg = new msgComponent({
+	        propsData: { client: true, text: this.field }
+	      });
+	      userMsg.$mount();
+	      this.$refs.list.appendChild(userMsg.$el);
+
+	      // sending user's message
+				params.append('q', this.field);
+
+				axios.post('/api/get-answer', params)
+				.then(response => {
+				
+					// rendering response
+					let serverMsg = new msgComponent({
+						propsData: { client: false, text: response.data.a }	
+					});
+					serverMsg.$mount();
+	      	this.$refs.list.appendChild(serverMsg.$el);
+				})
+				.catch(error => {
+	    		console.log(error);
+	  		});
+				
+				this.field = '';
+			}	
+		}
+	},
+	computed: {
+		isEmpty: function() {
+			if (!this.field.trim()) {
+				return true;
+			}
+			else {
+				return false;
+			}
 		}
 	}
 })
